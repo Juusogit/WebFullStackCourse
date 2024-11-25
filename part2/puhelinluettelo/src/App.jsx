d92a9f14829e5fbb68bf0ef3f31bcaaed87eb6fc
Juusogit <potteson@hotmail.com>
2024-11-25 04:22:47 +0200

    part2.11

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const FilterForm = ({ filter, handleFilterChange }) => (
   <div>
@@ -28,16 +29,19 @@ const PersonList = ({ persons }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-    { name: 'Jompaunder', number: '044-123420' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setNewName('')
+      })
+  }, [])
 
   const addName = (event) => {
     setNewName(event.target.value)
