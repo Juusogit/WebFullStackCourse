c44510ca964fd8a6cc6995ee69e46842386fa178
Juusogit <potteson@hotmail.com>
2024-12-13 18:28:16 +0200

    part4btyostoa

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
-let MONGO_URI = process.env.MONGO_URI
+let MONGO_URI = process.env.NODE_ENV === 'test'
+? process.env.TEST_MONGO_URI
+: process.env.MONGO_URI
 let PORT = process.env.PORT
 
 module.exports = {
