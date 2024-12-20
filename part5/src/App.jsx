d6826123f59059c0bcae55580a55861a497f46f3
Juusogit <potteson@hotmail.com>
2024-12-20 05:38:51 +0200

    part5tyostoa

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -4,12 +4,17 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')   
+  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = (event) => {event.preventDefault()    
+    console.log('logging in with', username, password)}
 
   return (
     <div>
