33362b23086045b1a3f5c7e77fca18f3e65138e7
Juusogit <potteson@hotmail.com>
2024-12-21 22:48:50 +0200

    part5tyostoa

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -6,17 +8,31 @@ import loginService from './services/login'
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [newBlog, setNewBlog] = useState('')
-  const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')   
   const [password, setPassword] = useState('')
-  const [user, setUser] = usestate(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll()
+      .then(blogs => {
+        setBlogs( blogs )
+    })  
   }, [])
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      content: newBlog
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewBlog('')
+      })
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -39,7 +55,7 @@ const App = () => {
   const loginForm = () => (      
     <form onSubmit={handleLogin}>        
     <div>          
-      username            
+      username
       <input            
       type="text"            
       value={username}            
@@ -68,13 +84,26 @@ const App = () => {
     </form>
   )
 
-return(
+
+return (
   <div>
+    <h1>Blogs</h1>
+    <Notification message={errorMessage}/>
+
+    {!user && loginForm()}
+    {user && <div>
+      <p>{user.name} logged in</p>
+      {blogForm()}
+    </div>
+    }
+    <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
+    <Footer />
+  </div>
   )
 }
 
